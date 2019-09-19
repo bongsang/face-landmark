d3d12d883302975009c36820a8c55ee2481b754f
Bongsang Kim <happykbs@gmail.com>
2019-09-20 06:17:44 +0900

    updated

--- landmarks_dataset.py
+++ landmarks_dataset.py
@@ -10,7 +10,7 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
-
+import cv2
 
 class LandmarksDataset(Dataset):
     """Face Landmarks dataset."""
@@ -34,9 +34,10 @@ class LandmarksDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
+        image_name = self.landmarks_frame.iloc[idx, 0]
+        image_path = os.path.join(self.root_dir, image_name)
+        image = cv2.imread(image_path)
+        image_data = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # if image has an alpha color channel, get rid of it
         if image.shape[2] == 4:
@@ -45,8 +46,8 @@ class LandmarksDataset(Dataset):
         landmarks = self.landmarks_frame.iloc[idx, 1:]
         landmarks = np.array([landmarks])
         landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
-        print(f'LandmarksDataset shape, image={image.shape}, landmarks={landmarks.shape}')
+        sample = {'image': image_data, 'landmarks': landmarks, 'name': image_name}
+        print(f'LandmarksDataset shape, file={image_name}, image={image_data.shape}, landmarks={landmarks.shape}')
 
         if self.transform:
             sample = self.transform(sample)
